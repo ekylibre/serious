c713400418c9ce0b7d61d0000e2a024ab7924d7e
croussy <clroussy@laposte.net>
2015-07-09 17:55:51 +0200

    Loan controller

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,20 +1,32 @@
 class LoansController < BaseController
-  before_action 'init'
   layout 'loan'
 
   def new
-    @loan = Loan.new
-    @loan.borrower = @current_participant
-    @loan.lender = @participant
-    puts @loan.inspect
-    #redirect_to action: :show, id: params[:id]
-  end
-
-  protected
-  def init
     @participant = Participant.find(params[:lender_id])
     @current_participation = Participation.find_by(user_id: current_user.id)
     @current_participant = Participant.find(@current_participation.participant_id)
     #@current_participant = Participant.find(16)
+
+    @loan = Loan.new
+    @loan.borrower = @current_participant
+    @loan.lender = @participant
+  end
+
+  def create
+    @loan = Loan.new(loan_params)
+    respond_to do |format|
+      if @loan.save
+        format.html { redirect_to @loan, notice: 'Loan was successfully created.' }
+        format.json { render :show, status: :created, location: @loan }
+      else
+        format.html { render :new }
+        format.json { render json: @loan.errors, status: :unprocessable_entity }
+      endle
+    end
+  end
+
+  private
+  def loan_params
+    params.require(:loan).permit(:borrower_id, :lender_id, :amount, :turns_count, :interest_percentage, :insurance_percentage)
   end
 end
